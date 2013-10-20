05654a41d3d6e80203c5896b65b004c887983611
Adam Ryman <adamryman@gmail.com>
2013-10-20 02:59:17 -0700

    Adding api key and getting 3day forcast from wunderground

--- shellWeather.py
+++ shellWeather.py
@@ -1,6 +1,16 @@
 import sys
+import json
+import urllib2
 
 if __name__ =='__main__':
   if len(sys.argv) < 2 or not sys.argv[1].isdigit():
     sys.exit(1)
   zipcode = sys.argv[1]
+
+  apikey = (open('api', 'r')).readline()
+
+  getURL = json.load(urllib2.urlopen('http://api.wunderground.com/api/' + apikey + '/geolookup/q/' + zipcode + '.json'))['location']['requesturl'].split(".")[0]
+
+  fullJson = json.load(urllib2.urlopen('http://api.wunderground.com/api/' + apikey + '/forecast/q/' + getURL + '.json'))
+
+  forecasts = fullJson['forecast']['simpleforecast']['forecastday']
