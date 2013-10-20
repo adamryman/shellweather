bb727c2c635a2300011862d27c6de50a42e68d86
Adam Ryman <adamryman@gmail.com>
2013-10-20 03:22:18 -0700

    Created an list of list of spaces

--- shellWeather.py
+++ shellWeather.py
@@ -2,6 +2,8 @@ import sys
 import json
 import urllib2
 
+screen = [[' ' for j in range(80)] for k in range(50)]
+
 if __name__ =='__main__':
   if len(sys.argv) < 2 or not sys.argv[1].isdigit():
     sys.exit(1)
@@ -13,4 +15,4 @@ if __name__ =='__main__':
 
   fullJson = json.load(urllib2.urlopen('http://api.wunderground.com/api/' + apikey + '/forecast/q/' + getURL + '.json'))
 
-  forecasts = fullJson['forecast']['simpleforecast']['forecastday']
+  threeDayForecast = fullJson['forecast']['simpleforecast']['forecastday']
